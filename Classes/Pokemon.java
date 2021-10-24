f1d49725b6c4de397d95964483bf70bdb831b949
Helzta <sebbebergstrom@hotmail.com>
2021-10-24 14:36:57 +0200

    The beninging of Pokemon

---
+++ Classes/Pokemon.java
@@ -0,0 +1,6 @@
+package Classes;
+public class Pokemon
+{
+    String name;
+    int maxHP, currentHP, atk, spatk, def, spdef, speed, maxExp, currentExp;
+}
---
+++ Program.java
@@ -0,0 +1,7 @@
+import Classes.*;
+public class Program
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
